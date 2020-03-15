d773bcf128327ecbb2bb9538427abd84851754f7
Faktor 10 <oreillyross@gmail.com>
2020-03-15 15:09:13 +0100

    using material-ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./styles.css";
+import PantlerAppBar from './components/PantlerAppBar'
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello Pantler</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <PantlerAppBar/>
     </div>
   );
 }
